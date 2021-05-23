42f8dbec3f93b48caab36b927267a205bbb55832
florianldt <florian.ludot@gmail.com>
2021-05-23 05:36:32 +0200

    Create contact.tsx

---
+++ pages/contact.tsx
@@ -0,0 +1,13 @@
+// #region Local Imports
+import { Layout, TabBar } from '@Components';
+// #endregion Local Imports
+
+function Contact(): JSX.Element {
+    return (
+        <Layout>
+            <TabBar />
+        </Layout>
+    );
+}
+
+export default Contact;
