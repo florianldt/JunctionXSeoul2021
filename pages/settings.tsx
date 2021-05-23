d9ff631f2f58bbc7612380fdba3152c330bde9d9
florianldt <florian.ludot@gmail.com>
2021-05-23 05:36:37 +0200

    Create settings.tsx

---
+++ pages/settings.tsx
@@ -0,0 +1,13 @@
+// #region Local Imports
+import { Layout, TabBar } from '@Components';
+// #endregion Local Imports
+
+function Settings(): JSX.Element {
+    return (
+        <Layout>
+            <TabBar />
+        </Layout>
+    );
+}
+
+export default Settings;
