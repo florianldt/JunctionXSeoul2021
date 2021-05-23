3f506a8739ea980d4c7071ba4a7f0fe75513d3b0
florianldt <florian.ludot@gmail.com>
2021-05-23 04:56:08 +0200

    generate icons components

---
+++ src/Icons/Analysis.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+function SvgAnalysis(props: React.SVGProps<SVGSVGElement>) {
+    return (
+        <svg
+            width={18}
+            height={18}
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+        >
+            <path
+                d="M16 0H2C.9 0 0 .9 0 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V2c0-1.1-.9-2-2-2zM5 14c-.55 0-1-.45-1-1V8c0-.55.45-1 1-1s1 .45 1 1v5c0 .55-.45 1-1 1zm4 0c-.55 0-1-.45-1-1V5c0-.55.45-1 1-1s1 .45 1 1v8c0 .55-.45 1-1 1zm4 0c-.55 0-1-.45-1-1v-2c0-.55.45-1 1-1s1 .45 1 1v2c0 .55-.45 1-1 1z"
+                fill="#000"
+            />
+        </svg>
+    );
+}
+
+export default SvgAnalysis;
---
+++ src/Icons/index.tsx
@@ -0,0 +1,4 @@
+export { default as Analysis } from './Analysis'
+export { default as Contact } from './Contact'
+export { default as Home } from './Home'
+export { default as Settings } from './Settings'
